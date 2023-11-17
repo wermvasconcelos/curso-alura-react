e7e67a456cc89cd891715902675afb005c64bda7
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-11-17 09:12:36 -0300

    feat: componente "campo texto"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   
   return (
     <div className="App">
       <Banner/>
+      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+      <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
     </div>
   );
 }
 
 export default App;
+ 
