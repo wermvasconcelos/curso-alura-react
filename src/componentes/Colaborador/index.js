9bb407e22f604f20d9c1ba97a199e248f38e9f9b
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-11-17 15:08:10 -0300

    feat: componente colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/wermvasconcelos.png' alt='wermvasconcelos'/>
+            </div>
+            <div className='rodape'>
+                <h4>Wermenson Vasconcelos</h4>
+                <h5>Líder Técnico</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
