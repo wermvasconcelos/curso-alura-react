4b6c6b5a3fd72785f527220ba5cbd669d23c9b5f
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-11-17 14:10:31 -0300

    feat: componente time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Fomulario';
+import Time from './componentes/Time';
 
 function App() {
   
@@ -14,6 +15,9 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionar(colaborador)}/>
+      <Time nome="Programação"/>
+      <Time nome="Front-End"/>
+      <Time nome="Data Science"/>
     </div>
   );
 }
