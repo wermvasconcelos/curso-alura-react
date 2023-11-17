7c53d34b32916ecd424a1132613a6cc7b9e7aaeb
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-11-17 11:26:46 -0300

    feat: componente botão

--- src/componentes/Fomulario/index.js
+++ src/componentes/Fomulario/index.js
@@ -1,28 +1,35 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
 
 const Formulario = () => {
 
     const times = [
-        'Programação',
+        'Programação', 
         'Front-End',
         'Data Science',
         'Devops',
         'UX e Design',
         'Mobile',
         'Inovação e Gestão'
-
     ]
+
+    const aoSalvar = () => {
+        console.log("quero morrer")
+    }
 
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
                 <ListaSuspensa label="Time" itens={times}/>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
