53f95102d219c3663afbb02b795a22d1b7624f30
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-11-17 13:46:06 -0300

    feat: novos useStates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Fomulario';
 
 function App() {
   
+  const [colaboradores, setColaboradores] = useState([]) 
+  const aoNovoColaboradorAdicionar = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner/>
-      <Formulario/>
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionar(colaborador)}/>
     </div>
   );
 }
