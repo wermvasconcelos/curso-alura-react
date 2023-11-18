faf2c275947372c7e14220db3d5f04dd8c9e131a
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-11-18 19:30:48 -0300

    feat: state de times

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,11 +1,11 @@
 import Colaborador from '../Colaborador'
 import './Time.css'
 
-const Time = ({ time, colaboradores, aoDeletar }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
 
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <input value={time.corSecundaria} type='color' className='input-cor'/>
+            <input value={time.corSecundaria} onChange={evento => mudarCor(evento.target.value,time.nome)} type='color' className='input-cor'/>
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
